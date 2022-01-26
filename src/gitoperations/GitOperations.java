fec8c0bcf59ff8402eb50c57217239213a6df267
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2022-01-26 12:41:08 -0500

    version 2

--- src/gitoperations/GitOperations.java
+++ src/gitoperations/GitOperations.java
@@ -16,7 +16,7 @@ public class GitOperations {
      */
     public static void main(String[] args) 
     {
-        System.out.println("to learn github operations");
+        System.out.println("to learn github operations commit");
     }
     
 }
